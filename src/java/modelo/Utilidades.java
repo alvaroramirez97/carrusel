f2673aa948b9d64f7f72fb5b645f348945478402
DAW202 <DAW202@DESKTOP-UN17071>
2020-02-19 19:43:22 +0100

    Cambios

---
+++ src/java/modelo/Utilidades.java
@@ -0,0 +1,42 @@
+
+package modelo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DAW202
+ */
+public class Utilidades {
+    
+    	public static ArrayList<String> getArchivos(String sDirectorio) throws IOException{
+		ArrayList<String> archivos = new ArrayList<String>();
+		
+                File dir = new File(sDirectorio);
+                String[] ficheros = dir.list();
+                if (ficheros != null) { 
+                        for (int x=0;x<ficheros.length;x++){
+                            if ( ficheros[x].endsWith(".jpg") || ficheros[x].endsWith(".png") ||
+                                    ficheros[x].endsWith(".gif") ){
+                                String fichero="carrusel"+File.separator+ficheros[x];
+                                archivos.add(fichero);
+                            }
+                        }
+                }
+		return archivos;		
+	}
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : index
+    Created on : 17-feb-2020, 21:19:11
+    Author     : DAW202
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <a href="carrusel">arrancar</a>
+    </body>
+</html>
